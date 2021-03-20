8f84b54f4277d76d849398fcac27c6d4d0dd6ad4
tomohiro1234 <gongyezhihong732@gmail.com>
2021-03-20 11:48:10 +0900

    Initial Commit

---
+++ SoccerQuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoccerQuizApp
+//
+//  Created by 宮野智宏 on 2021/03/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
